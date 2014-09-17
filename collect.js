16196e425d52f6b953828bd4489adc6f1d610c9b
Cédric Finance <cedric.finance@finandric.com>
2014-09-17 18:47:32 +0000

    Send data to thingspeak

--- collect.js
+++ collect.js
@@ -1,10 +1,28 @@
 var teleinfo = require("teleinfo");
 var util = require('util');
+var ThingSpeakClient = require('thingspeakclient');
+
+var key = process.env["THINGSPEAK_KEY"] 
+if (typeof(key) === 'undefined') {
+  console.error("ERROR: Environment variable THINGSPEAK_KEY is not set.") 
+  process.exit(1)
+}
+
+var client = new ThingSpeakClient();
+client.attachChannel(14248, { writeKey: key });
 
 var trameEvents = teleinfo("/dev/ttyAMA0");
 
+var lastSent;
+
 trameEvents.on('tramedecodee', function (data) {
-	console.log(util.inspect(data));
+  var consommation = { field1: data.HCHC, field2: data.HCHP, field3: data.IINST, field4: data.PAPP}
+  var currentDate = new Date();
+  if (!lastSent || ((currentDate - lastSent) > 15 * 1000)) {
+    client.updateChannel(14248, consommation);   
+    console.log(util.inspect(data));
+    lastSent = currentDate;
+  } 
 });
 
 trameEvents.on('error', function (err) {
