78e62cc48287c00c56730f9e14be482e8515d97a
Cédric Finance <cedric.finance@finandric.com>
2015-01-11 17:38:18 +0100

    Add an environment variable for the channel id

--- collect.js
+++ collect.js
@@ -4,14 +4,20 @@ var teleinfo = require("teleinfo");
 var util = require('util');
 var ThingSpeakClient = require('thingspeakclient');
 
-var key = process.env["THINGSPEAK_KEY"] 
-if (typeof(key) === 'undefined') {
-  console.error("ERROR: Environment variable THINGSPEAK_KEY is not set.") 
-  process.exit(1)
+function get_env(name) {
+  var value = process.env[name]
+  if (typeof(value) === 'undefined') {
+    console.error("ERROR: Environment variable "+name+" is not set.");
+    process.exit(1);
+  }
+  return value;
 }
 
+var key = get_env("THINGSPEAK_KEY");
+var channel_id = get_env("CHANNEL_ID");
+
 var client = new ThingSpeakClient();
-client.attachChannel(14248, { writeKey: key });
+client.attachChannel(channel_id, { writeKey: key });
 
 var trameEvents = teleinfo("/dev/ttyAMA0");
 
@@ -21,7 +27,7 @@ trameEvents.on('tramedecodee', function (data) {
   var consommation = { field1: data.HCHC, field2: data.HCHP, field3: data.IINST, field4: data.PAPP}
   var currentDate = new Date();
   if (!lastSent || ((currentDate - lastSent) > 15 * 1000)) {
-    client.updateChannel(14248, consommation);   
+    client.updateChannel(channel_id, consommation);
     console.log(util.inspect(data));
     lastSent = currentDate;
   } 
